906bc493adea9cd3cc1f4b325744da2ca6826062
Yuto Nakamura <maron07222010@gmail.com>
2021-12-14 01:04:15 +0900

    gif途中

--- lzw.c
+++ lzw.c
@@ -7,7 +7,7 @@
 #define CODE_SIZE 100
 
 typedef struct node {
-  char c[100];
+  char c[100000];
   bool registerd;
 } Node;
 
@@ -39,8 +39,16 @@ void node_append(Node* node, size_t* size, char* value) {
   }
 }
 
+int return0xnum(char buf[]) {
+  int num = 0;
+  for (int i = 0; i < 8; i++) {
+    num += buf[i] * 1 << (7 - i);
+  }
+  return num;
+}
+
 int main(int argc, char* argv[]) {
-  Node n[2000];
+  Node *n = malloc(sizeof(Node)*300);
   n[0] = node_init("0");
   n[1] = node_init("1");
   n[2] = node_init("2");
@@ -48,7 +56,24 @@ int main(int argc, char* argv[]) {
   n[4] = node_init("4");
   n[5] = node_init("5");
   size_t size = 6;
-  char code[CODE_SIZE] = "010101010";
+  char code[CODE_SIZE];
+  int width = 30;
+  int height = 30;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      if (10 <= i && i < 20 && 10  <= j && j < 20) {
+        code[i * height + j] = '1';
+      } else {
+        code[i * height + j] = '1';
+      }
+      printf("%d",code[i*height+j]);
+    }
+    printf("\n");
+  }
+
+  char bitsize[CODE_SIZE + 2];
+  int bitlength = 3;
+  bitsize[0] = bitlength;
   char ans[CODE_SIZE + 2];
   size_t anssize = 0;
   ans[0] = 4;
@@ -67,22 +92,76 @@ int main(int argc, char* argv[]) {
       s[j] = code[i + j];
       j++;
     }
+    printf("j = %d\n",j);
     if (i + j == strlen(code) && node_find(n, size, s) != -1) {
       ans[anssize + 1] = node_find(n, size, s);
+      bitsize[anssize + 1] = bitsize[anssize + 2] = bitlength;
       ans[anssize + 2] = 5;
       anssize++;
       break;
     } else {
       i += strlen(s) - 2;
+      printf("i = %d,%s\n",i,s);
       node_append(n, &size, s);
       s[j - 1] = '\0';
       ans[anssize + 1] = node_find(n, size, s);
+      bitsize[anssize + 1] = bitlength;
+      if (size - 1 >= 1 << bitlength) {
+        bitlength++;
+      }
       anssize++;
     }
   }
+
   for (int i = 0; i < anssize + 2; i++) {
     printf("%d,", ans[i]);
   }
   printf("\n");
+  for (int i = 0; i < anssize + 2; i++) {
+    printf("%d,", bitsize[i]);
+  }
+  printf("\n");
+  char buf[8];
+  int bufindex = 7;
+  int imageboard[256];
+  int imageindex = 0;
+  for (int i = 0; i < anssize + 2; i++) {
+    char x = ans[i];
+    char y[bitsize[i]];
+    for (int j = 0; j < bitsize[i]; j++) {
+      if (x & 1 << (bitsize[i] - 1 - j)) {
+        y[j] = 1;
+      } else {
+        y[j] = 0;
+      }
+    }
+    printf("\n");
+    for (int j = 0; j < bitsize[i]; j++) {
+      if (bufindex < 0) {  // bufが満杯
+        for (int k = 0; k < 8; k++) {
+        }
+        imageboard[imageindex] = return0xnum(buf);
+        ++imageindex;
+        bufindex = 7;
+      }
+      buf[bufindex] = y[bitsize[i] - 1 - j];
+      bufindex--;
+    }
+    if (i == anssize + 1) {
+      for (int j = 0; j <= bufindex; j++) {
+        buf[j] = 0;
+      }
+      imageboard[imageindex] = return0xnum(buf);
+      imageindex++;
+    }
+  }
+  FILE* fp;
+  fp = fopen("giftest.dat","w");
+  for (int i = 0; i < imageindex; i++) {
+    fprintf(fp,"%d,",imageboard[i]);
+  }
+  fprintf(fp,"\n%d",imageindex);
+  fclose(fp);
+  free(n);
   return EXIT_SUCCESS;
 }
